74a4d30ad9fe66ea3f1914d19ac8481d0b8f4b25
Harsh517-tech415 <harsh.choudhary@rampwin.com>
2024-10-09 16:40:44 +0530

    Update wms.controller.ts

--- src/controllers/wms.controller.ts
+++ src/controllers/wms.controller.ts
@@ -9,12 +9,18 @@ export const getWmsMaster = async (req: RequestWithUser, res: Response) => {
   try {
     const { master } = req.params;
     const requestUser: IUser = req.user;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = Number(page * limit - limit);
+
     let fetchedData: unknown[] = [];
     switch (master) {
       case "country":
         {
           (fetchedData = await Country.findAll({
             where: { company_code: requestUser.company_code },
+            offset: skip,
+            limit: limit,
           })) as unknown[] as ICountry[];
         }
         break;
