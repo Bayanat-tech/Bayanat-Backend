287e80c5d92fb9c49a8947f9cd6a35a59b2cffd1
Bayanat-tech <support@bayanattechnology.com>
2024-10-11 18:35:21 +0530

    fixes

--- src/routes/wms/gm_wms.routes.ts
+++ src/routes/wms/gm_wms.routes.ts
@@ -4,7 +4,10 @@ import {
   deleteCountries,
   updateCountry,
 } from "../../controllers/wms/country_wms.controller";
-import { createdepartment, updatedepartment } from "../../controllers/wms/department_wms.controller";
+import {
+  createdepartment,
+  updatedepartment,
+} from "../../controllers/wms/department_wms.controller";
 const router = express.Router();
 
 router.post("/country", createCountry);
@@ -13,7 +16,6 @@ router.put("/country", updateCountry);
 //-------------department------------
 router.post("/department", createdepartment);
 router.put("/department", updatedepartment);
-
 
 router.post("/country/delete", deleteCountries);
 
