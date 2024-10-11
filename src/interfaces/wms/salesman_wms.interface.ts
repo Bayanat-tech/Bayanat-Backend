0ceade6dd757d61cde9bf7e2aaf8d42d0ac802b9
pankajswamivaishnav-rampwin <pankaj.vaishnav@rampwin.com>
2024-10-11 15:08:17 +0530

    feature/get_api
    In this commit make three api
    1. territory_api
    2. currency_api
    3. salesman_api

---
+++ src/interfaces/wms/salesman_wms.interface.ts
@@ -0,0 +1,9 @@
+export interface ISalesman {
+    company_code: string;
+    salesman_code: string;
+    salesman_name: string;
+    created_at?: Date;
+    created_by?: string;
+    updated_at?: Date;
+    updated_by?: string;
+}
---
+++ src/models/wms/salesman_wms.model.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from "sequelize";
+import { ISalesman } from "../../interfaces/wms/salesman_wms.interface";
+import { sequelize } from "../../database/connection";
+import constants from "../../helpers/constants";
+
+class Salesman extends Model<ISalesman> {};
+
+Salesman.init({
+  company_code:{
+    type: DataTypes.STRING(5),
+    allowNull: false,
+    primaryKey: true,
+  },
+  salesman_code: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+    primaryKey: true,
+  },
+  salesman_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  updated_by: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  created_by: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+},
+{
+    sequelize,
+    modelName: "Salesman",
+    tableName: constants.TABLE.MS_SALESMAN,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    })
+
+    export default Salesman;
