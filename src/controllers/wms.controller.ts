dd4b88e271f20c472bc12ba82436b1d685c5021a
pankajswamivaishnav-rampwin <pankaj.vaishnav@rampwin.com>
2024-10-15 17:48:37 +0530

    make get all activity

--- src/controllers/wms.controller.ts
+++ src/controllers/wms.controller.ts
@@ -10,11 +10,15 @@ import { ICurrency } from "../interfaces/wms/currency_wms.interface";
 // Importing models for WMS master data
 import Country from "../models/wms/country_wms.model";
 import Department from "../models/wms/department_wms.model";
-import Currency from "../models/wms/currency_wms.model"
+import Currency from "../models/wms/currency_wms.model";
 import Territory from "../models/wms/territory_wms.model";
 import Salesman from "../models/wms/salesman_wms.model";
 import Site from "../models/wms/site_wms.model";
 import Storage from "../models/wms/storage_wms.model";
+
+// --- Database sequelize import ---
+import { sequelize } from "../database/connection";
+import { QueryTypes } from "sequelize";
 
 // Retrieves master data (country, department, territory, etc.) with optional pagination based on the `master` type.
 export const getWmsMaster = async (req: RequestWithUser, res: Response) => {
@@ -25,61 +29,90 @@ export const getWmsMaster = async (req: RequestWithUser, res: Response) => {
     const limit = Number(req.query.limit) || 10;
     const skip = Number(page * limit - limit);
     let fetchedData: unknown[] = [];
-    const paginationOptions = limit ? { offset: skip, limit: limit } : {}; 
-    console.log("master: ----->>>>>>" + master);
+    const paginationOptions = limit ? { offset: skip, limit: limit } : {};
     switch (master) {
       case "country":
         {
           (fetchedData = await Country.findAll({
             where: { company_code: requestUser.company_code },
-            ...paginationOptions
+            ...paginationOptions,
           })) as unknown[] as ICountry[];
         }
-        break; 
+        break;
       case "department":
-        {          
+        {
           (fetchedData = await Department.findAll({
-            where: { company_code: requestUser.company_code},
-            ...paginationOptions
+            where: { company_code: requestUser.company_code },
+            ...paginationOptions,
           })) as unknown[] as IDepartment[];
-        } 
-        break; 
-        case "territory": {
+        }
+        break;
+      case "territory":
+        {
           (fetchedData = await Territory.findAll({
             where: { company_code: requestUser.company_code },
-            ...paginationOptions
+            ...paginationOptions,
           })) as unknown[] as ITerritory[];
         }
         break;
-        case "currency": {
+      case "currency":
+        {
           (fetchedData = await Currency.findAll({
             where: { company_code: requestUser.company_code },
-           ...paginationOptions
+            ...paginationOptions,
           })) as unknown[] as ICurrency[];
         }
         break;
-        case "salesman":{
-          (fetchedData = await Salesman.findAll({
+      case "salesman":
+        {
+          fetchedData = await Salesman.findAll({
             where: { company_code: requestUser.company_code },
-            ...paginationOptions
-          }))
+            ...paginationOptions,
+          });
         }
         break;
-        case "site" :
-          {
-            (fetchedData = await Site.findAll({
-              where: { company_code: requestUser.company_code },
-             ...paginationOptions
-            }))
-          }
-          break;
-          case "storage":
-            {
-              (fetchedData = await Storage.findAll({
-                where: { company_code: 'BSG' },
-               ...paginationOptions
-              }))
-            }
+      case "site":
+        {
+          fetchedData = await Site.findAll({
+            where: { company_code: requestUser.company_code },
+            ...paginationOptions,
+          });
+        }
+        break;
+      case "storage":
+        {
+          fetchedData = await Storage.findAll({
+            where: { company_code: requestUser.company_code },
+            ...paginationOptions,
+          });
+        }
+        break;
+
+      case "activity_billing": {
+        const query = `
+                SELECT
+                  P.PRIN_NAME,
+                  B.ACT_CODE,
+                  A.ACTIVITY,
+                  B.JOBTYPE
+                FROM
+                  MS_PRINCIPAL P
+                JOIN
+                  MS_ACTIVITY_BILLING B ON P.PRIN_CODE = B.PRIN_CODE
+                JOIN
+                  MS_ACTIVITY A ON B.ACT_CODE = A.ACTIVITY_CODE
+                WHERE
+                  P.COMPANY_CODE = :companyCode;
+              `;
+
+        const activityBillingData = await sequelize.query(query, {
+          replacements: { companyCode: requestUser.company_code },
+          type: QueryTypes.SELECT,
+        });
+
+        fetchedData = activityBillingData;
+        break;
+      }
     }
     res.status(constants.STATUS_CODES.OK).json({
       success: true,
@@ -96,71 +129,93 @@ export const getWmsMaster = async (req: RequestWithUser, res: Response) => {
 };
 
 // Delete master data (country, department, territory, etc.) with optional pagination based on the `master` type.
-export const deleteWmsMaster = async (req:RequestWithUser, res:Response) =>{
+export const deleteWmsMaster = async (req: RequestWithUser, res: Response) => {
   try {
     const { master } = req.params;
     const requestUser: IUser = req.user;
-    const {dept_code, country_code, territory_code, curr_code, salesman_code} = req.body;
+    const {
+      dept_code,
+      country_code,
+      territory_code,
+      curr_code,
+      salesman_code,
+    } = req.body;
     switch (master) {
       case "country":
         {
-          if(!country_code || country_code.length === 0){
+          if (!country_code || country_code.length === 0) {
             throw new Error("countryCode is required");
           }
           await Country.destroy({
-            where: { company_code: requestUser.company_code, country_code: country_code},
+            where: {
+              company_code: requestUser.company_code,
+              country_code: country_code,
+            },
           });
         }
         break;
       case "department":
         {
-          if(!dept_code || dept_code.length === 0){
+          if (!dept_code || dept_code.length === 0) {
             throw new Error("departmentCode is required");
           }
           await Department.destroy({
-            where: { company_code: requestUser.company_code, dept_code: dept_code},
+            where: {
+              company_code: requestUser.company_code,
+              dept_code: dept_code,
+            },
           });
         }
         break;
       case "territory":
         {
-          if(!territory_code || territory_code.length === 0){
+          if (!territory_code || territory_code.length === 0) {
             throw new Error("territoryCode is required");
           }
           await Territory.destroy({
-            where: { company_code: requestUser.company_code, territory_code: territory_code},
+            where: {
+              company_code: requestUser.company_code,
+              territory_code: territory_code,
+            },
           });
         }
         break;
       case "currency":
         {
-          if(!curr_code || curr_code.length === 0){
+          if (!curr_code || curr_code.length === 0) {
             throw new Error("currencyCode is required");
           }
           await Currency.destroy({
-            where: { company_code: requestUser.company_code, curr_code:curr_code },
+            where: {
+              company_code: requestUser.company_code,
+              curr_code: curr_code,
+            },
           });
         }
         break;
-      case "salesman":{
-        if(!salesman_code || salesman_code.length === 0){
-          throw new Error("salesmanCode is required");
+      case "salesman":
+        {
+          if (!salesman_code || salesman_code.length === 0) {
+            throw new Error("salesmanCode is required");
+          }
+          await Salesman.destroy({
+            where: {
+              company_code: requestUser.company_code,
+              salesman_code: salesman_code,
+            },
+          });
         }
-        await Salesman.destroy({
-          where: { company_code: requestUser.company_code, salesman_code:salesman_code },
-        });
-      }
-      break;
+        break;
     }
     res.status(constants.STATUS_CODES.OK).json({
       success: true,
       message: `${master} is successfully deleted`,
     });
-   return;
-  } catch (error:any) {
+    return;
+  } catch (error: any) {
     res
       .status(constants.STATUS_CODES.BAD_REQUEST)
       .json({ success: false, message: error.message });
     return;
   }
-}
+};
---
+++ src/interfaces/wms/activity_billing_wms.interface.ts
@@ -0,0 +1,28 @@
+export interface IActivityBilling {
+    PRIN_CODE: string;
+    act_code: string;
+    wip_code:string;
+    cost:number;
+    income_code: string;
+    bill_amount: number;
+    jobtype: string;
+    company_code: string;
+    freeze_flag: string;
+    mandatory_flag: string;
+    validate_flag: string;
+    uoc: string;
+    moc: number;
+    moc1: string;
+    moc2: string;
+    cust_code: string;
+    start_point: string;
+    end_point: string;
+    customer_type: string;
+    vtype_code: string;
+    serial_no: number;
+    serial_no2: number;
+    created_at?: Date;
+    created_by?: string;
+    updated_at?: Date;
+    updated_by?: string;
+}
