d26cca4d4da705bcee592ab8f4c99482af13af1f
pankajswamivaishnav-rampwin <pankaj.vaishnav@rampwin.com>
2024-10-16 14:33:34 +0530

    make getActivityBillingDataByCompanyAndPrincipal api

--- src/routes/wms.routes.ts
+++ src/routes/wms.routes.ts
@@ -3,7 +3,7 @@
   import { getWmsMaster, deleteWmsMaster } from "../controllers/wms.controller";
   import gmWmsRouter from "./wms/gm_wms.routes";
   import { checkUserAuthorization } from "../middleware/checkUserAthorization";
-
+ import { getActivityBillingDataByCompanyAndPrincipal } from "../controllers/wms/activity_wms.controller";
   const router = express.Router();
 
   router.get(
@@ -12,7 +12,13 @@
     checkUserAuthorization,
     getWmsMaster
   );
-
+  router.get(
+    "/:master/:companyCode/:principalCode",
+    passport.authenticate("jwt", { session: false }),
+    checkUserAuthorization,
+    getActivityBillingDataByCompanyAndPrincipal 
+  );
+  
   router.use(
     "/gm",
     passport.authenticate("jwt", { session: false }),
