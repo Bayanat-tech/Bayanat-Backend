2c54868e7ee36d4365ee44286b576285ffef33a4
unknown <admin@DESKTOP-98SGIRG.amlsdc.local>
2024-10-24 15:11:56 +0530

    COMMIT

--- src/interfaces/wms/harmonize.interface.ts
+++ src/interfaces/wms/harmonize.interface.ts
@@ -2,10 +2,6 @@
 export interface IHarmonize{
     harm_code: string;
     harm_desc: string;
-    // short_desc: string;
-    // uom:string;
-    // permit_reqd:string;
-    // unit:string;
     company_code?: string;
     updated_at?: Date;
     updated_by?: string;
--- src/models/wms/harmonize_code.model.ts
+++ src/models/wms/harmonize_code.model.ts
@@ -12,34 +12,15 @@ Harmonize.init(
       allowNull: false,
       primaryKey: true,
     },
+
     harm_desc: {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
-    // uom: {
-    //   type: DataTypes.STRING(5),
-    //   allowNull: true,
-    // },
-    // unit: {
-    //     type: DataTypes.STRING(5),
-    //     allowNull: true,
-    //   },
     company_code: {
       type: DataTypes.STRING(20),
       allowNull: false,
     },
-    // short_desc: {
-    //   type: DataTypes.STRING(255),
-    //   allowNull: true,
-    // },
-    // permit_reqd: {
-    //   type: DataTypes.STRING(255),
-    //   allowNull: true,
-    // },
-    // updated_at: {
-    //   type: DataTypes.DATE,
-    //   allowNull: false,
-    // },
     updated_by: {
       type: DataTypes.STRING(50),
       allowNull: false,
@@ -48,10 +29,10 @@ Harmonize.init(
       type: DataTypes.STRING(20),
       allowNull: false,
     },
-    // created_at: {
-    //   type: DataTypes.DATE,
-    //   allowNull: false,
-    // },
+    short_desc: "",
+    uom: "",
+    permit_reqd: "",
+    unit: ""
   },
   {
     sequelize,
